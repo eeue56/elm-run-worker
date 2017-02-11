f5fd414fc92e6c7f3764e332e6db57eaf6f73b47
eeue56 <enalicho@gmail.com>
2017-02-11 19:48:59 +0100

    get ready for release

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # elm-run
-Run Elm files 
+
+Compile and run Elm files in worker mode in a single command. 
+Useful for testing Node-based Elm apps!
+
+# Install
+
+```
+npm install -g elm-run
+```
+
+### Usage
+
+```
+noah@Noahs-MacBook-Pro ~/d/elm-run> elm-run --help
+Options:
+  -v, --verbose  Print all messages out                         [default: false]
+  -o, --output   Store the generated code in the given output file
+                                                                 [default: null]
+  -h, --help     Show help                                             [boolean]
+
+Examples:
+  /usr/local/bin/elm-run Main.elm  Compile then run Main.elm
+```
+
+
+## Example
+
+### Compile + run `example/Main.elm` 
+
+```
+elm-run example/Main.elm --output elm.js
+```
+
+
+### Compile + run `example/Main.elm` and store it in `elm.js`
+
+```
+elm-run example/Main.elm --output elm.js
+```
+
+
+### Compile + run `example/Main.elm` `example/OtherMain.elm` and store it in `elm.js`
+
+```
+elm-run example/Main.elm example/OtherMain.elm --output elm.js
+```
--- index.js
+++ index.js
@@ -2,21 +2,56 @@
 
 const yargs = require('yargs');
 
-const compileToString = require('node-elm-compiler').compileToString;
+const compiler = require('node-elm-compiler');
+const compileToString = compiler.compileToString;
+const compile = compiler.compile;
+
 const yargv = yargs
     .example('$0 Main.elm', 'Compile then run Main.elm')
     .alias('v', 'verbose')
     .default('v', false)
     .describe('v', 'Print all messages out')
+    .alias('o', 'output')
+    .default('o', null)
+    .describe('o', 'Store the generated code in the given output file')
     .help('h')
     .alias('h', 'help')
     .argv;
 
 
 const files = yargv._;
-const options = {
-    output: ".js"
+
+
+const dontStore = (files, isVerbose) => {
+    const options = {
+        output: ".js",
+        verbose: isVerbose
+    };
+
+    compileToString(files, options).then(function(body){
+        // make it runable cause Elm is not wrapped properly
+        var Elm = eval(body + "\nmodule.exports = module.exports;");
+
+        Object.keys(Elm).forEach(function(name){
+            if (yargv.verbose) console.log('Starting ', name, "...");
+            Elm[name].worker();
+        })
+    }).catch(function(error){
+        console.error("Something went wrong!");
+        console.error(error);
+    });    
 };
+
+
+const store = (files, outputFile, isVerbose) => {
+    const options = {
+        output: outputFile,
+        verbose: isVerbose
+    };
+
+    compile(files, options);
+};
+
 
 if (files.length < 1){
     console.error('Please give at least one file!');
@@ -25,15 +60,9 @@ if (files.length < 1){
     if (yargv.verbose) console.log('Compiling: ', files);
 }
 
-compileToString(files, options).then(function(body){
-    var Elm = eval(body + "\nmodule.exports = module.exports;");
-    console.log(Elm);
+if (yargv.o === null || typeof yargv.o == "undefined") {
+    dontStore(files, yargv.verbose);
+} else {
+    store(files, yargv.o, yargv.verbose);
+}
 
-    Object.keys(Elm).forEach(function(name){
-        if (yargv.verbose) console.log('Starting ', name, "...");
-        Elm[name].worker();
-    })
-}).catch(function(error){
-    console.error("Something went wrong!");
-    console.error(error);
-});
